2d33152337cafd363d1e1ad619617971d32d7ddb
jefferson10147 <jeffersonmontilla.98@gmail.com>
2021-02-23 15:54:08 -0400

    Improving search by expression

--- modules/searches.py
+++ modules/searches.py
@@ -19,7 +19,7 @@ def process_students_data(students_data, number_of_students=10, random_data=True
 
     elif len(students_data) > 1 and not random_data:
         messages_list.append(
-            f'Here {number_of_students} are some of the best results')
+            f'Here are {number_of_students} are some of the best results')
         selection = students_data[0:number_of_students]
 
     else:
@@ -32,15 +32,26 @@ def process_students_data(students_data, number_of_students=10, random_data=True
     return messages_list
 
 
+def filter_data(result):
+    if result['lastname'] == result['second_lastname'] or result['name'] == result['second_name']:
+        return False
+
+    return True
+
+
 def search_by_expression(expression):
     api_endpoint = f'{expression}'
     response = requests.get(''.join([api_url, api_endpoint]))
     if response.status_code == 200:
+        max_results = 15
+        results = response.json()[:max_results]
+        filtered_results = list(filter(filter_data, results))
         messages = process_students_data(
-            response.json(),
-            number_of_students=5,
+            filtered_results,
+            number_of_students=len(filtered_results),
             random_data=False
         )
+
         return messages
 
 
