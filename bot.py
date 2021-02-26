92f9b38f8ce4cb22bca80671a1b8ac34efc4b1f5
jefferson10147 <jeffersonmontilla.98@gmail.com>
2021-02-26 11:57:12 -0400

    Changin loggin module config

--- bot.py
+++ bot.py
@@ -15,7 +15,7 @@ from modules.searches import (
 
 logging.basicConfig(
     format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
-    level=logging.DEBUG
+    level=logging.INFO
 )
 PORT = int(config('port'))
 BOT_TOKEN = config('bot_token')
